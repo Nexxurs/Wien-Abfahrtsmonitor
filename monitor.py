0fe148ba85abc70ad2fa80c9943d179d67fc55ef
Unknown <p.uebermasser@gmx.at>
2017-10-03 17:40:49 +0200

    Reformating and No Monitors in JSON Bugfix (hopefully)

--- monitor.py
+++ monitor.py
@@ -28,13 +28,20 @@ def replaceUmlaut(s):
 
 
 def main(argv):
+    global apikey
+    global lcdUsage
+    global consoleUsage
+    global st
+    global maxError
+    global config
+
     apikey = False
-    apiurl = 'https://www.wienerlinien.at/ogd_realtime/monitor?rbl={rbl}&sender={apikey}'
+
     lcdUsage = True
     consoleUsage = True
     st = 10
     maxError = 10
-    global config
+
     config = config = configparser.ConfigParser()
     try:
         opts, args = getopt.getopt(argv, "hk:t:c:", ["help", "config="])
@@ -64,10 +71,6 @@ def main(argv):
         usage()
         sys.exit()
 
-    if lcdUsage == consoleUsage:
-        debug = eq
-    else:
-        debug = uneq
 
     rbls = []
     configrbls = config["Settings"]["rbls"].split(',')
@@ -85,64 +88,87 @@ def main(argv):
     if lcdUsage:
         configLCD()
 
+    print("------------------------------------------")
+    print("New Start: LCD={} Console={} RBLs={}".format(lcdUsage, consoleUsage, configrbls))
+
     x = 0
     while True:
         for rbl in rbls:
-            url = apiurl.replace('{apikey}', apikey).replace('{rbl}', rbl.id)
+            useRBL(rbl)
 
-            if not debug(consoleUsage, lcdUsage):
-                raise Exception("Change in Usage: Console=%s, LCD=%s" % (consoleUsage, lcdUsage))
 
-            try:
-                r = requests.get(url)
-                if r.status_code == 200:
-                    json = r.json()['data']['monitors']
+def useRBL(rbl):
+    apiurl = 'https://www.wienerlinien.at/ogd_realtime/monitor?rbl={rbl}&sender={apikey}'
 
-                    soonest = json[0]
+    url = apiurl.replace('{apikey}', apikey).replace('{rbl}', rbl.id)
+    r = None
 
-                    if len(json) > 1:
-                        for i in range(1,len(json)):
-                            if json[i]['lines'][0]['departures']['departure'][0]['departureTime']['countdown'] < \
-                                    soonest['lines'][0]['departures']['departure'][0]['departureTime']['countdown']:
-                                soonest = json[i]
+    try:
+        r = requests.get(url)
+        if r.status_code == 200:
+            json = r.json()['data']['monitors']
 
-                    rbl.line = soonest['lines'][0]['name']
-                    rbl.station = soonest['locationStop']['properties']['title']
-                    rbl.direction = soonest['lines'][0]['towards']
-                    rbl.time = soonest['lines'][0]['departures']['departure'][0]['departureTime']['countdown']
-                    if consoleUsage:
-                        dumpRBL(rbl)
-                    if lcdUsage:
-                        lcdShow(rbl)
-                    x = 0
-                    time.sleep(st)
-            except KeyboardInterrupt:
-                print("User exit by Keyboard Interrupt")
+            if len(json) <= 0:
+                print("Problematic JSON: {}".format(r.json()))
                 if lcdUsage:
                     lcd.clear()
-                sys.exit(0)
-            except urlexceptions.MaxRetryError as e:
-                print("Max Retries Exception!")
-                if lcdUsage:
-                    lcd.clear()
-                    lcd.message("Connection Error!\nWill try again shortly")
+                    lcd.message("Problematic JSON!")
                 time.sleep(1)
-            except Exception as e:
-                x += 1
-                if x > maxError:
-                    print(str(x) + "FATAL ERROR")
-                    print(e)
-                    if lcdUsage:
-                        lcd.clear()
-                        lcd.message("FATAL ERROR \nCheck the Logs")
-                    raise
-                else:
-                    print(str(x) + " ERROR " + str(e))
-                    if lcdUsage:
-                        lcd.clear()
-                        lcd.message("ERROR-{}\nCheck the Logs".format(type(e)))
-                    time.sleep(1)
+            else:
+                soonest = json[0]
+                if len(json) > 1:
+                    for i in range(1, len(json)):
+                        if json[i]['lines'][0]['departures']['departure'][0]['departureTime']['countdown'] < \
+                                soonest['lines'][0]['departures']['departure'][0]['departureTime']['countdown']:
+                            soonest = json[i]
 
+                rbl.line = soonest['lines'][0]['name']
+                rbl.station = soonest['locationStop']['properties']['title']
+                rbl.direction = soonest['lines'][0]['towards']
+                rbl.time = soonest['lines'][0]['departures']['departure'][0]['departureTime']['countdown']
+                x = 0
+                if consoleUsage:
+                    dumpRBL(rbl)
+                if lcdUsage:
+                    lcdShow(rbl)
+                time.sleep(st)
+
+        else:
+            print("Request Status Code: {}".format(r.status_code))
+            if lcdUsage:
+                lcd.clear()
+                lcd.message("Request Status Code!\n{}".format(r.status_code))
+    except KeyboardInterrupt:
+        print("User exit by Keyboard Interrupt")
+        if lcdUsage:
+            lcd.clear()
+        sys.exit(0)
+    except urlexceptions.MaxRetryError as e:
+        print("Max Retries Exception!")
+        if lcdUsage:
+            lcd.clear()
+            lcd.message("Connection Error!\nWill try again shortly")
+        time.sleep(1)
+    except Exception as e:
+        x += 1
+        if x > maxError:
+            print("FATAL ERROR")
+            print("Type: {}".format(type(e)))
+            if r is not None:
+                json = r.json()
+                if json is not None:
+                    print("With JSON:")
+                    print(str(json))
+            if lcdUsage:
+                lcd.clear()
+                lcd.message("FATAL ERROR \nCheck the Logs")
+            raise
+        else:
+            print(str(x) + " ERROR " + str(type(e)) + " - " + str(e))
+            if lcdUsage:
+                lcd.clear()
+                lcd.message("ERROR-{}\nCheck the Logs".format(type(e)))
+            time.sleep(1)
 
 def configLCD():
     import Adafruit_CharLCD as LCD
@@ -181,15 +207,6 @@ def usage():
     print('  -c, --config\tconfig file with API Key and rbl numbers')
     print('optional arguments:')
     print('  -h, --help\tshow this help')
-
-
-def eq(b1, b2):
-    return b1 == b2
-
-
-def uneq(b1, b2):
-    return b1 != b2
-
 
 if __name__ == "__main__":
     main(sys.argv[1:])
